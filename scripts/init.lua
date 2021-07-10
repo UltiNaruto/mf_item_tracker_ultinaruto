e9ad1668ba6b1ea0786a9a63ddc2fc191a8b3ae7
UltiNaruto <zenaruto@ymail.com>
2021-07-10 17:26:34 +0200

    Version 1.0.2
    Added Split Lock Levels variant
    Added black outline to Lock Level images

---
+++ scripts/init.lua
@@ -0,0 +1,20 @@
+IS_SPLIT_LOCK_LEVELS = string.find(Tracker.ActiveVariantUID, "split_lock_levels") ~= nil
+
+-- Items
+
+Tracker:AddItems("items/common.json")
+if IS_SPLIT_LOCK_LEVELS then
+	Tracker:AddItems("items/lock_levels/split.json")
+else
+	Tracker:AddItems("items/lock_levels/standard.json")
+end
+
+-- Layouts
+
+if IS_SPLIT_LOCK_LEVELS then
+	Tracker:AddLayouts("layouts/split_lock_levels/tracker.json")
+else
+	Tracker:AddLayouts("layouts/standard/tracker.json")
+end
+
+Tracker:AddLayouts("layouts/broadcast.json")
